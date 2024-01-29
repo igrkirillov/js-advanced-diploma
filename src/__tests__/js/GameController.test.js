a808ee5412b11ab009a9d8a18d70dad8d11144bd
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-01-29 07:40:28 +0300

    доработка 1

--- src/__tests__/js/GameController.test.js
+++ src/__tests__/js/GameController.test.js
@@ -1,30 +1,60 @@
 import GameController from "../../js/GameController.js";
 import GamePlay from "../../js/GamePlay.js";
 import GameStateService from "../../js/GameStateService.js";
+import GameState from "../../js/GameState.js";
+import themes from "../../js/themes";
 jest.mock("../../js/GamePlay.js");
 jest.mock("../../js/GameStateService.js");
+jest.mock("../../js/GameState.js");
 
 
 beforeEach(() => {
   GamePlay.mockClear();
   GameStateService.mockClear();
+  GameState.mockClear();
 })
 
 describe("GameController module", () => {
-  test ("loadGame вызывает load объекта stateService " +
-    "и drawUi объекта gamePlay " +
-    "и redrawPositions объекта gamePlay", () => {
+  test ("load объекта stateService выполнен успешно и возвращает новый GameState", () => {
+    const currentThemeOfGameState = themes.mountain;
+    GameState.mockImplementation(() => {
+      return {
+        currentTheme: currentThemeOfGameState,
+      };
+    });
+    GameStateService.mockImplementation(() => {
+      return {
+        load: () => {
+          return new GameState();
+        },
+      };
+    });
+
+    const controller = new GameController(new GamePlay(), new GameStateService());
+    const mockGamePlay = GamePlay.mock.instances[0];
+
+    // главный вызов
+    controller.loadGame();
+
+    expect(mockGamePlay.drawUi).toHaveBeenCalledWith(currentThemeOfGameState);
+    expect(mockGamePlay.redrawPositions).toHaveBeenCalled();
+  });
+  test ("load объекта stateService выбрасывает исключение", () => {
+    const errorMessage = "Error loading new state";
+    GameStateService.mockImplementation(() => {
+      return {
+        load: () => {
+          throw new Error(errorMessage);
+        },
+      };
+    });
 
     const controller = new GameController(new GamePlay(), new GameStateService());
 
     // главный вызов
     controller.loadGame();
 
-    const mockGamePlay = GamePlay.mock.instances[0];
-    const mockStateService = GameStateService.mock.instances[0];
-
-    expect(mockStateService.load).toHaveBeenCalled();
-    expect(mockGamePlay.drawUi).toHaveBeenCalled();
-    expect(mockGamePlay.redrawPositions).toHaveBeenCalled();
+    expect(GamePlay.showError).toHaveBeenCalledWith(
+      `Упс! Не удалось загрузить игру из памяти! Причина: ${errorMessage}`);
   });
 });
